2d8b5dd96589e9cf841c327467ce08f43d7e1987
Forerunner-Mods <talk2edfitz@gmail.com>
2017-09-20 21:41:15 +0100

    Initial Commit

--- TrailerModCreator/frmMain.Designer.cs
+++ TrailerModCreator/frmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TrailerModCreator
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "frmMain";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
